5d7eb497d72899a90d7c81a715b2c4dea84177c4
Cello Ward <celloward@gmail.com>
2019-11-29 11:01:10 -0600

    Destroy duplicate migrations

--- db/migrate/20191127195833_add_foreign_keys_to_leaderships.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKeysToLeaderships < ActiveRecord::Migration[5.2]
-  def change
-    add_foreign_key :leaderships, :ministries, 
-    add_foreign_key :leaderships, :leaders, :bigint 
-    add_index :leaderships, :ministries_id
-    add_index :leaderships, leaders_id
-  end
-end
