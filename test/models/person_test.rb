d0c72a28f160d7fe9717b422886b39bcf74181bc
Cello Ward <celloward@gmail.com>
2019-12-19 10:46:46 -0600

    Tighten up spouse tests

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -120,36 +120,89 @@ class PersonTest < ActiveSupport::TestCase
   end
 
   #Associations with Self(Spouse)
-  test "can have spouse" do
-    @person.save
-    @spouse.save
-    @person.spouse = @spouse
-    assert @person.spouse == @spouse
-  end
 
-  test "can have no spouse" do
-    assert @person.spouse.blank?
-  end
-
-  test "cannot have more than one living spouse" do
+  setup do
     @person.save
     @spouse.save
     @secondspouse = Person.new(first_name: "Second", last_name: "Person")
     @secondspouse.save
-    @person.spouse = @spouse
-    assert_raise { @person.spouses = @secondspouse }
-    @spouse.living = false
-    @person.spouses << @secondspouse
+  end
+
+  test "can be married" do
+    @person.marry(@spouse)
+    assert_equal @person.spouses.married, @spouse
+    assert_equal @spouse.spouses.married, @person
+  end
+
+  test "can be unmarried" do
+    assert_equal @person.spouses.married, nil
     assert @person.valid?
   end
 
   test "cannot have self as spouse" do
-    assert_raise { @person.spouse << @person }
+    assert_raise { @person.spouses << @person }
   end
 
   test "cannot have child as spouse" do
     @person.children << @child
-    assert_raise { @person.spouse << @person.child }
+    assert_raise { @person.spouses << @person.child }
+  end
+
+  test "cannot have duplicate current marriages to same person" do
+    @person.marry(@spouse)
+    assert_raise { @person.spouses << @spouse }
+    assert_raise { @spouse.spouses << @person }
+    @spouse.die
+    assert_equal @person.spouses, @spouse
+    assert_equal @person.spouses.married, nil
+    assert_equal @spouse.spouses, @person
+    assert_equal @spouse.spouses.married, nil
+    @person << @secondspouse
+    assert_equal @person.spouses.married, nil
+    @person.marry(@secondspouse)
+    assert @person.valid?
+    assert_equal @person.spouses.married, @secondspouse
+    assert_equal @spouse.spouses, @person
+    assert_equal @secondspouse.spouses.married, @person
+  end
+
+  test "cannot have more than one current spouse" do
+    @person.marry(@spouse)
+    assert_raise { @person.marry(@secondspouse) }
+    assert_raise { @spouse.marry(@secondspouse) }
+    assert_equal @person.spouses, @spouse
+    assert_eqaul @spouse.spouses, @person
+  end
+
+  test "can have former spouse and current spouse" do
+    @person.spouses << @spouse
+    @person.marry(@secondspouse)
+    assert @person.valid?
+    assert_equal @person.spouses.married, @secondspouse
+    assert_eqaul @person.spouses.count, 2
+    assert_equal @spouse.spouses.married, @person
+    assert_equal @spouse.spouses.count, 1
+    assert_equal @secondspouse.spouses.count, 1
+  end
+
+  test "can have former spouse thorugh death" do
+    @person.marry(@secondspouse)
+    @secondspouse.die
+    assert_equal @person.spouses.married, nil
+    assert_equal @seconspouse.spouses.married, nil
+    assert_equal @person.spouses.all, @secondspouse
+    assert_equal @secondspouse.spouses.all, @person
+  end
+
+  test "can create former spouses through divorce" do
+    @person.marry(@spouse)
+    assert_equal @person.spouses.married, @spouse
+    assert_equal @spouse.spouses.married, @person
+    @person.divorce(@spouse)
+    assert_equal @person.spouses.married, nil
+    assert_eqaul @spouse.spouses.married, nil
+    assert_equal @person.spouses.all, @spouse
+    assert_equal @spouse.spouses.all, @person
   end
   
   #Associations with Ministry
