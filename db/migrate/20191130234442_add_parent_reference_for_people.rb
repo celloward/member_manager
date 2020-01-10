2dbca081242319309e8f8c86ef5fdd6210d9b590
Cello Ward <celloward@gmail.com>
2020-01-10 13:47:34 -0600

    Clean up migrations

--- db/migrate/20191130234442_add_parent_reference_for_people.rb
+++ db/migrate/20191130234442_add_parent_reference_for_people.rb
@@ -1,5 +1,5 @@
 class AddParentReferenceForPeople < ActiveRecord::Migration[5.2]
   def change
-    add_reference :people, :parent, index: true
+    add_reference :people, :parent
   end
 end
--- db/migrate/20191201015835_add_spouse_reference_to_people.rb
+++
@@ -1,5 +0,0 @@
-class AddSpouseReferenceToPeople < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :people, :spouse, index: true
-  end
-end
